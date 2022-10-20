e2be6476aede47edbeb2390cef084446a344c05c
DmitryRubtsov53 <2578390@mail.ru>
2022-10-20 22:26:30 +0500

    Solved task2 Transport OOP2.

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,50 @@ public class Main {
         System.out.println("Домашнее задание 2 урока ООП 2 **********************************************************");
         System.out.println();
 
-        System.out.println("Задание 1 и 2." + "\n");
+        System.out.println("Список автомобилей:" + "\n");
 
-        Car car1 = new Car("Lada", "Granta", 2015, "Россия", "желтый", 1.7);
-        car1.printCars();
-        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный", 3.0);
-        car2.printCars();
-        Car car3 = new Car("BMW", "Z8", 2021, "Германия", "черный", 3.0);
-        car3.printCars();
-        Car car4 = new Car("Kia", "Sportage", 2018, "Южная Корея", "красный", 2.4);
-        car4.printCars();
-        Car car5 = new Car("Hyundai", "Avante", 2016, "Южная Корея", "оранжевый", 1.6);
-        car5.printCars();
-        Car car6 = new Car(null, null, 0, null, null, 0);
-        car6.printCars();
+        Car [] car = new Car[6];
+
+        car[0] = new Car(null, null, 0, null, null, 0,"ландо", 5, "АКПП", "A102AA102", "лето");
+        car[0].printCars();
+        car[1] = new Car("Lada", "Granta", 2015, "Россия", "желтый", 1.7, "купе", 5, "", "P555CX199", "");
+        car[1].printCars();
+        car[2] = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный", 3.0, "купе", 0, null, "", "");
+        car[2].printCars();
+        car[3] = new Car("BMW", "Z8", 2021, "Германия", "черный", 3.0, "кабриалет", 0, null, null, "зима");
+        car[3].printCars();
+        car[4] = new Car("Kia", "Sportage", 2018, "Южная Корея", "красный", 2.4,null, 0, null, "B153EC107",null);
+        car[4].printCars();
+        car[5] = new Car("Hyundai", "Avante", 2016, "Южная Корея", "оранжевый", 1.6, null, 2, null, "", "зима");
+        car[5].printCars();
+
+        System.out.println();
+        System.out.println("Список авто после сезонной замены резины и проверки Госномеров:" + "\n");
+
+        String seazon = "зима";   // текущий сезон
+        replacementTire (car, seazon);
+        checkNumbers (car);
+        //car[0].printCars(); car[1].printCars(); car[2].printCars(); car[3].printCars(); car[4].printCars(); car[5].printCars();
+
     }
+// methods *************************************************************************************************************
+    public static Car[] replacementTire (Car[] car, String seazon) {
+
+        for (int i = 0; i < car.length; i++) {
+            if (car[i].getTiresType().equals(seazon)) {
+                continue;
+            } else car[i].setTiresType(seazon);
+        }
+    return car;
+    }
+    public static Car[]  checkNumbers (Car[] car) {
+
+    //    for (int i = 0; i < car.length; i++) {
+    //        if (car[i].getTiresType().equals(seazon)) {
+    //            continue;
+    //        } else car[i].setTiresType(seazon);
+    //    }
+        return car;
+    }
+
 }
--- src/Transport/Car.java
+++ src/Transport/Car.java
@@ -2,32 +2,94 @@ package Transport;
 
 public class Car {
 
-    String brand;
-    String model;
+    final String brand;
+    final String model;
+    final int yearOfIssue;
+    final String assemblyCountry;
+    final String bodyType;   // тип кузова
+    final int places;             // Количество мест
+
+    public String getBrand() { return brand; }
+    public String getModel() { return model; }
+    public int getYearOfIssue() { return yearOfIssue; }
+    public String getAssemblyCountry() { return assemblyCountry; }
+    public String getBodyType() { return bodyType; }
+    public int getPlaces() { return places; }
+    // ***************************************************************
     double engineVolume;
     String bodyColor;
-    int yearOfIssue;
-    String assemblyCountry;
+    String gearbox;         // коробка передач
+    String regNumber;          // Регистрационный номер
+    String tiresType;       // тип авторезины - летняя или зимняя
 
-    public Car (String brand, String model, int yearOfIssue, String assemblyCountry, String bodyColor, double engineVolume) {
-        if (brand == null) { this.brand = "default";
-        } else this.brand = brand;
-        if (model == null) { this.model = "default";
-        } else this.model = model;
+    public double getEngineVolume() { return engineVolume; }
+    public void setEngineVolume(double engineVolume) {
         if (engineVolume == 0) { this.engineVolume = 1.5;
         } else this.engineVolume = engineVolume;
-        if (bodyColor == null) { this.bodyColor = "белый";
-        } else this.bodyColor = bodyColor;
-        if (yearOfIssue == 0) { this.yearOfIssue = 2000;
-        } else this.yearOfIssue = yearOfIssue;
-        if (assemblyCountry == null) { this.assemblyCountry = "default";
-        } else this.assemblyCountry = assemblyCountry;
-
-
     }
-       public void printCars() {
-            System.out.println(brand + " " + model + ", " + yearOfIssue + "-го года выпуска, сборка " +
-                    assemblyCountry + ", " + "цвет " + bodyColor + ", объем двигателя - " + engineVolume + " литра.");
+    public String getBodyColor() { return bodyColor;  }
+
+    public void setBodyColor(String bodyColor) {
+        if (bodyColor == null || bodyColor.isEmpty() || bodyColor.isBlank()) { this.bodyColor = "белый";
+        } else this.bodyColor = bodyColor;
+    }
+    public String getGearbox() { return gearbox;  }
+    public void setGearbox(String gearbox) {
+        this.gearbox = gearbox;
+    }
+    public String getRegNumber() { return regNumber; }
+    public void setRegNumber(String regNumber) {
+        if (regNumber == null || regNumber.isEmpty() || regNumber.isBlank()) { this.regNumber = "не выдан";
+        }  else  this.regNumber = regNumber;
+    }
+    public String getTiresType() { return tiresType; }
+    public void setTiresType(String tiresType) {
+        if (tiresType == null || tiresType.isEmpty() || tiresType.isBlank()) { this.tiresType = "летняя";
+        } else this.tiresType = tiresType;
+    }
+
+    // Constructor ****************************************************
+    public Car (String brand, String model, int yearOfIssue, String assemblyCountry, String bodyColor, double engineVolume,
+                String bodyType, int places,String gearbox, String regNumber, String tiresType ) {
+        if (brand == null || brand.isEmpty() || brand.isBlank()) {
+            this.brand = "default";
+            } else this.brand = brand;
+        if (model == null || model.isEmpty() || model.isBlank()) {
+            this.model = "default";
+            } else this.model = model;
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+            } else this.engineVolume = engineVolume;
+        if (bodyColor == null || bodyColor.isEmpty() || bodyColor.isBlank()) {
+            this.bodyColor = "белый";
+            } else this.bodyColor = bodyColor;
+        if (yearOfIssue == 0) { this.yearOfIssue = 2000;
+            } else this.yearOfIssue = yearOfIssue;
+        if (assemblyCountry == null || assemblyCountry.isEmpty() || assemblyCountry.isBlank()) {
+            this.assemblyCountry = "default";
+            } else this.assemblyCountry = assemblyCountry;
+        if (bodyType == null || bodyType.isEmpty() || bodyType.isBlank()) {
+            this.bodyType = "хэнчбек";
+            }  else  this.bodyType = bodyType;
+        if (places == 0) { this.places = 5;
+            } else this.places = places;
+        if (regNumber == null || regNumber.isEmpty() || regNumber.isBlank()) {
+            this.regNumber = "не выдан";
+            }  else  this.regNumber = regNumber;
+        if (gearbox == null || gearbox.isEmpty() || gearbox.isBlank()) {
+            this.gearbox = "МКПП";
+            }  else this.gearbox = gearbox;
+        if (tiresType == null || tiresType.isEmpty() || tiresType.isBlank()) {
+            this.tiresType = "летняя";
+            } else this.tiresType = tiresType;
+        }
+
+// methods ***********************************************************
+    public void printCars() {
+            System.out.println(brand + " " + model + ", " + yearOfIssue + "-го года, сборка " +
+              assemblyCountry + ", " + "цвет " + bodyColor + ", V двигателя - " + engineVolume + " л., кузов - " +
+              bodyType + ", количество мест " + places + ", Госномер " + regNumber + ", " + gearbox +
+              ", тип авторезины - " +  tiresType + ".");
         }
 
 }
